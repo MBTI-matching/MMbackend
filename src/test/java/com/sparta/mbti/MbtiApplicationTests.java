687b7aa118c3172ed6e05b9c5dff2778c0f587e2
good4y <kmosh1212@naver.com>
2021-12-19 23:20:11 +0900

    첫 커밋

---
+++ src/test/java/com/sparta/mbti/MbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
