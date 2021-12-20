d544741eae34d64b6026686309463049e37c4ec2
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-12-20 15:21:51 +0900

    첫 commit

---
+++ src/main/java/com/sparta/mbti/controller/CommentController.java
@@ -0,0 +1,9 @@
+package com.sparta.mbti.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+}
