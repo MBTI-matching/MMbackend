b4d140e48197811cbc5f364c27f5ac31f5da2710
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-07 10:39:55 +0900

    main에서 env 뺌 & test용 api 뺌

--- src/main/java/com/sparta/mbti/controller/HomeController.java
+++ src/main/java/com/sparta/mbti/controller/HomeController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.List;
 
 @RequiredArgsConstructor
