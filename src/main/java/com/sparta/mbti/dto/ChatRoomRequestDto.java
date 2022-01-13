e2fb31028e34c03aac9a7bd99d83e04c7b12c82c
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-13 16:19:26 +0900

    채팅룸 생성: notion에 적힌 원칙대로 수정(guestId -> email)

--- src/main/java/com/sparta/mbti/dto/ChatRoomRequestDto.java
+++ src/main/java/com/sparta/mbti/dto/ChatRoomRequestDto.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class ChatRoomRequestDto {
     @Column
-    private Long guestId; // entity에 guestId가 long이라고 되어있어서 entity쪽에 맞춤
+    private String guestId; // guestEmail로 바꿔쓰길 제의
 
     @Column
     private String guestMbti;
