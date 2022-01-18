48aa293423f103d5ab856cbdcd91cab92c315206
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-18 10:26:08 +0900

    www-data -> ubuntu

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -7,7 +7,7 @@
 function find_idle_profile()
 {
     # curl 결과로 연결할 서비스 결정
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{https_code}" https://sixzombies.shop/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://sixzombies.shop/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
