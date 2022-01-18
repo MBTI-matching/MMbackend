40f50e3fac413674857ee8961b44d561428a0a19
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-18 10:16:58 +0900

    www-data -> ubuntu

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -7,13 +7,13 @@
 function find_idle_profile()
 {
     # curl 결과로 연결할 서비스 결정
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{https_code}" https://sixzombies.shop/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=set2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s https://sixzombies.shop/profile)
     fi
 
     # IDLE_PROFILE : nginx와 연결되지 않은 profile
