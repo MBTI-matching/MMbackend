509f87fa4a95759de5447d5727f65ec7fd4cfe59
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-18 14:56:15 +0900

    Nginx reload 위치 바꿈

--- scripts/Nswitch.sh
+++ scripts/Nswitch.sh
@@ -19,8 +19,8 @@ echo "> 전환할 Port: $IDLE_PORT"
 echo "> Port 전환"
 echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
 
-PROXY_PORT=$(curl -s https://sixzombies.shop/profile)
-echo "> Nginx Current Proxy Port: $PROXY_PORT"
-
 echo "> Nginx Reload"
 sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
+
+PROXY_PORT=$(curl -s https://sixzombies.shop/profile)
+echo "> Nginx Current Proxy Port: $PROXY_PORT"
