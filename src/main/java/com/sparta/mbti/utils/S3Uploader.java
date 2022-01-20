5d2a602e0206623d03d53017e31ec955bbb670d0
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-20 13:42:39 +0900

    S3Uploader - sentry 추가

--- src/main/java/com/sparta/mbti/utils/S3Uploader.java
+++ src/main/java/com/sparta/mbti/utils/S3Uploader.java
@@ -3,6 +3,7 @@ package com.sparta.mbti.utils;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sparta.mbti.sentry.SentrySupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,11 +22,13 @@ import java.util.UUID;
 public class S3Uploader {
 
     private final AmazonS3Client amazonS3Client;
+    private final SentrySupport sentrySupport;
 
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;  // S3 버킷 이름
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        sentrySupport.logSimpleMessage("multipart file upload");
         File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
                 .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
 
@@ -34,6 +37,7 @@ public class S3Uploader {
 
     // S3로 파일 업로드하기
     private String upload(File uploadFile, String dirName) {
+        sentrySupport.logSimpleMessage("upload - uploadFile");
         String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
         String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
         removeNewFile(uploadFile);
@@ -42,12 +46,14 @@ public class S3Uploader {
 
     // S3로 업로드
     private String putS3(File uploadFile, String fileName) {
+        sentrySupport.logSimpleMessage("putS3 - uploadFile");
         amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
     // 로컬에 저장된 이미지 지우기
     private void removeNewFile(File targetFile) {
+        sentrySupport.logSimpleMessage("removeNewFile - targetFile");
         if (targetFile.delete()) {
             log.info("File delete success");
             return;
@@ -57,6 +63,7 @@ public class S3Uploader {
 
     // 로컬에 파일 업로드 하기
     private Optional<File> convert(MultipartFile file) throws IOException {
+        sentrySupport.logSimpleMessage("convert - uploadToLocal");
         File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
@@ -64,9 +71,6 @@ public class S3Uploader {
             }
             return Optional.of(convertFile);
         }
-
         return Optional.empty();
     }
-
-
 }
