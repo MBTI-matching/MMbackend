6b5369ce0bc816222f0931e1b1cf6412d4b5a21d
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2022-01-25 11:00:08 +0900

    sentry - error log 감소 & debug false & 0.1 rate & BG시험

--- scripts/Nswitch.sh
+++ scripts/Nswitch.sh
@@ -19,9 +19,6 @@ echo "> 전환할 Port: $IDLE_PORT"
 echo "> Port 전환"
 echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "> Nginx Reload"
-sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
-
 PROXY_PORT=$(curl -s https://sixzombies.shop/profile)
 echo "> Nginx Current Proxy Port: $PROXY_PORT"
 
